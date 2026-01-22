8d3259a3db563e361eee4a2fef176996de39fdeb
ongsalt <me@ongsa.lt>
2026-01-23 03:34:51 +0700

    refactor: enhance XDGToplevelListener with additional callbacks

--- Sources/Wayland/XDG/XDGTopLevel.swift
+++ Sources/Wayland/XDG/XDGTopLevel.swift
@@ -4,37 +4,52 @@ import Glibc
 
 // we should actually do codegen
 
+public struct XDGToplevelListener {
+    let configure: ((SIMD2<Int32>, [Any]) -> Void)? = nil
+    let close: (() -> Void)? = nil
+    let configureBounds: ((SIMD2<Int32>) -> Void)? = nil
+    let wmCapabilities: (([Any]) -> Void)? = nil
+}
+
 public class XDGTopLevel {
     let topLevel: OpaquePointer
-    private var listener: xdg_toplevel_listener
+    private let swiftListener: XDGToplevelListener?
+    private var listener: xdg_toplevel_listener?
 
-    public init(surface: XDGSurface, title: String = "App", appId: String = UUID().uuidString) {
+    public init(
+        surface: XDGSurface, title: String = "App", appId: String = UUID().uuidString,
+        listener swiftListener: XDGToplevelListener? = nil
+    ) {
         self.title = title
         self.appId = appId
         self.topLevel = xdg_surface_get_toplevel(surface.surface)
+        self.swiftListener = swiftListener
 
-        listener = xdg_toplevel_listener(
-            configure: { (data, topLevel, w: Int32, h: Int32, _: UnsafeMutablePointer<wl_array>?) in
-                let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
+        if swiftListener != nil {
+            listener = xdg_toplevel_listener(
+                configure: {
+                    (data, topLevel, w: Int32, h: Int32, _: UnsafeMutablePointer<wl_array>?) in
+                    let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
+                    this.swiftListener?.configure?(SIMD2(w, h), [])
+                },
+                close: { (data, topLevel) in
+                    let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
+                    this.swiftListener?.close?()
 
-            },
-            close: { (data, topLevel) in
-                let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
+                },
+                configure_bounds: { (data, topLevel, w: Int32, h: Int32) in
+                    let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
+                    this.swiftListener?.configureBounds?(SIMD2(w, h))
+                },
+                wm_capabilities: { (data, topLevel, wtf: UnsafeMutablePointer<wl_array>?) -> Void in
+                    let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
+                    this.swiftListener?.wmCapabilities?([])
+                }
+            )
 
-            },
-            configure_bounds: { (data, topLevel, w: Int32, h: Int32) in
-                let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
-
-            },
-            wm_capabilities: { (data, topLevel, wtf: UnsafeMutablePointer<wl_array>?) -> Void in
-                let this = Unmanaged<XDGTopLevel>.fromOpaque(data!).takeUnretainedValue()
-
-            }
-        )
-
-        // print(observer)
-        let this = Unmanaged.passUnretained(self).toOpaque()
-        xdg_toplevel_add_listener(topLevel, &listener, this)
+            let this = Unmanaged.passUnretained(self).toOpaque()
+            xdg_toplevel_add_listener(topLevel, &listener!, this)
+        }
     }
 
     public var title: String {
